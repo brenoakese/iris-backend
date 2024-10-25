cdbe1103831dd5e851720d43a3a36076df7066a9
brenoakese <amorimbreno1207@gmail.com>
2024-10-25 11:03:56 -0300

    Update SecurityConfig.java

--- src/main/java/com/springwebundf/securityjwtproject/infra/security/SecurityConfig.java
+++ src/main/java/com/springwebundf/securityjwtproject/infra/security/SecurityConfig.java
@@ -14,6 +14,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import static com.springwebundf.securityjwtproject.domain.user.Permissions.*;
+import static com.springwebundf.securityjwtproject.domain.user.Roles.COORDENADOR;
+import static com.springwebundf.securityjwtproject.domain.user.Roles.PROFESSOR;
+import static org.springframework.http.HttpMethod.*;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
@@ -30,10 +35,29 @@ public class SecurityConfig {
             .csrf(csrf -> csrf.disable())
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
+
                         .requestMatchers(HttpMethod.POST, "auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "auth/register").permitAll()
                         .requestMatchers(HttpMethod.POST, "auth/register/coordenador").permitAll()
                         .requestMatchers("/swagger-ui/*", "/v3/api-docs/**").permitAll()
+
+                        .requestMatchers(POST, "auth/login").permitAll()
+                        .requestMatchers(POST, "auth/register").permitAll()
+                        .requestMatchers(POST,"auth/register/coordenador").permitAll()
+
+                        .requestMatchers("/api/coordenador/**").hasRole(COORDENADOR.name())
+                        .requestMatchers(GET,"/api/coordenador/**").hasAuthority(COORDENADOR_READ.name())
+                        .requestMatchers(POST,"/api/coordenador/**").hasAuthority(COORDENADOR_CREATE.name())
+                        .requestMatchers(PUT,"/api/coordenador/**").hasAuthority(COORDENADOR_UPDATE.name())
+                        .requestMatchers(DELETE,"/api/coordenador/**").hasAuthority(COORDENADOR_DELETE.name())
+
+                        .requestMatchers("/api/professor/**").hasRole(PROFESSOR.name())
+                        .requestMatchers(GET,"/api/professor/**").hasAuthority(PROFESSOR_READ.name())
+                        .requestMatchers(POST,"/api/professor/**").hasAuthority(PROFESSOR_CREATE.name())
+                        .requestMatchers(PUT,"/api/professor/**").hasAuthority(PROFESSOR_UPDATE.name())
+                        .requestMatchers(DELETE,"/api/professor/**").hasAuthority(PROFESSOR_DELETE.name())
+
+
                     .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);

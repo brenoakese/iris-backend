a1958f09e17c7a484a058e2234f80a1664182cf4
Leonardo Brito Gomes <leonardo.gomes@cade.gov.br>
2024-10-25 12:35:00 -0300

    Configuração swagger auth

---
+++ src/main/java/com/springwebundf/securityjwtproject/config/OpenApiConfiguration.java
@@ -0,0 +1,21 @@
+package com.springwebundf.securityjwtproject.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+
+@OpenAPIDefinition(
+    info = @Info(title = "API CADE REST", version = "v1")
+)
+@SecurityScheme(
+    name = "bearerAuth",
+    type = SecuritySchemeType.HTTP,
+    scheme = "bearer",
+    bearerFormat = "JWT",
+    in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfiguration {
+    
+}

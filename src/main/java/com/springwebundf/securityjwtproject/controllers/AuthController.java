1d092ba9fd031c0e0695d89ceeac7d60e011e476
Luiz Gustavo <l19051997@gmail.com>
2024-11-19 09:59:12 -0300

    Corrigindo get mapping de rota de validação

--- src/main/java/com/springwebundf/securityjwtproject/controllers/AuthController.java
+++ src/main/java/com/springwebundf/securityjwtproject/controllers/AuthController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -132,9 +133,9 @@ public class AuthController {
         return null;
     }
 
-    @GetController(/validar-token)
-    public ResponseEntity<> validarToken(@RequestBody String token) {
-        if(tokenService.validarToken(token) != null) {
+    @GetMapping("/validar-token")
+    public ResponseEntity validarToken(@RequestBody String token) {
+        if(tokenService.validateToken(token) != null) {
             return ResponseEntity.ok().body(Collections.singletonMap("mensagem", "Token válido!"));
         } else {
             return ResponseEntity.badRequest().body(Collections.singletonMap("mensagem", "Token inválido"));
